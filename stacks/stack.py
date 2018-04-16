776d22a3d11a5c52f134d60051c61132beff265b
Dev Sanghvi <dys27@cornell.edu>
2018-04-16 11:11:16 -0400

    Update stack.py

--- stacks/stack.py
+++ stacks/stack.py
@@ -7,7 +7,7 @@ class Stack:
         self.items.append(item)
     def pop(self):
         return self.items.pop()
-    def peak(self):
+    def peek(self):
         return self.items[len(self.items)-1]
     def size(self):
         return len(self.items)
