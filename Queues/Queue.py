076189cc4399d462e9fc8ae166cf35f244e62b64
Dev Sanghvi <dys27@cornell.edu>
2018-10-27 18:55:28 -0400

    Update Queue.py

--- Queues/Queue.py
+++ Queues/Queue.py
@@ -1,7 +1,7 @@
 class Queue:
     def __init__(self):
         self.items=[]
-    def isEmpty(self):
+    def is_empty(self):
         return self.items==[]
     def enqueue(self,item):
         self.items.insert(0,item)
