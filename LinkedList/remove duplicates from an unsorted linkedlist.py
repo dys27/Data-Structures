0ad1e74f46902ffd6e52192b878ff8f0ed20671c
Dev Sanghvi <dys27@cornell.edu>
2018-12-18 14:47:36 +0530

    Update remove duplicates from an unsorted linkedlist.py

--- LinkedList/remove duplicates from an unsorted linkedlist.py
+++ LinkedList/remove duplicates from an unsorted linkedlist.py
@@ -1,76 +1,76 @@
-class node(object):
+class Node(object):
     def __init__(self,data):
         self.data=data
-        self.nextNode=None
-    def remove(self,data,previousNode):
+        self.next_node=None
+    def remove(self,data,previous_node):
         if self.data==data:
-            previousNode.nextNode=self.nextNode
+            previous_node.next_node=self.next_node
             del self.data
-            del self.nextNode
+            del self.next_node
         else:
-            if self.nextNode is not None:
-                self.nextNode.remove(data,self)
-class linkedlist(object):
+            if self.next_node is not None:
+                self.next_node.remove(data,self)
+class LinkedList(object):
     def __init__(self):
         self.head=None
         self.counter=0
-    def addFirst(self,data):
+    def add_first(self,data):
         self.counter+=1
-        newNode=node(data)
+        new_node=Node(data)
         if self.head is None:
-            self.head=newNode
+            self.head=new_node
         else:
-            newNode.nextNode=self.head
-            self.head=newNode
+            new_node.next_node=self.head
+            self.head=new_node
 
-    def addLast(self,data):
+    def add_last(self,data):
         if self.head is None:
-            self.addFirst(data)
+            self.add_first(data)
             return
         self.counter+=1
-        newNode=node(data)
-        currentNode=self.head
-        while currentNode.nextNode is not None:
-            currentNode=currentNode.nextNode
-        currentNode.nextNode=newNode
+        new_node=node(data)
+        current_node=self.head
+        while current_node.next_node is not None:
+            current_node=current_node.next_node
+        current_node.next_node=new_node
 
     def size(self):
         return self.counter
 
     def traverse(self):
-        currentNode=self.head
-        while currentNode is not None:
-            print "%d"%currentNode.data,
-            currentNode=currentNode.nextNode
+        current_node=self.head
+        while current_node is not None:
+            print "%d"%current_node.data,
+            current_node=current_node.next_node
 
     def remove(self,data):
         self.counter-=1
         if self.head:
             if self.head.data==data:
-                self.head=self.head.nextNode
+                self.head=self.head.next_node
             else:
                 self.head.remove(data,self.head)
     def reverse(self):
-        currentNode=self.head
-        tempPrev=None
-        while currentNode is not None:
-            tempNext=currentNode.nextNode
-            currentNode.nextNode=tempPrev
-            tempPrev=currentNode
-            currentNode=tempNext
-        self.head=currentNode
+        current_node=self.head
+        temp_prev=None
+        while current_node is not None:
+            temp_next=current_node.next_node
+            current_node.next_node=temp_prev
+            temp_prev=current_node
+            current_node=temp_next
+        self.head=current_node
 
     def duplicate(self):
         dictionary={}
-        currentNode=self.head
-        prevNode=None
-        while currentNode is not None:
-            if currentNode.data not in dictionary:
-                dictionary[currentNode.data]=1
-                prevNode=currentNode
-                currentNode=currentNode.nextNode        
+        current_node=self.head
+        prev_node=None
+        while current_node is not None:
+            if current_node.data not in dictionary:
+                dictionary[current_node.data]=1
+                prev_node=current_node
+                current_node=current_node.next_node        
             else:
-                prevNode.nextNode=currentNode.nextNode
-                del currentNode.data
-                del currentNode.nextNode
-                currentNode=prevNode.nextNode
+                prev_node.next_node=current_node.next_node
+                del current_node.data
+                del current_node.next_node
+                current_node=prev_node.next_node
